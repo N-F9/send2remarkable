90dcf4922f3b0e73759eea4bd0ac3d6426f86152
Nick Ferguson <ndferguson@protonmail.com>
2024-12-27 10:54:52 -0500

    backups

--- s2r.py
+++ s2r.py
@@ -1,10 +1,22 @@
 import os
 import subprocess
 import argparse
+import datetime
 
-# [ ] backup
 # [ ] export
-# [ ] support folders
+
+def backup_remarkable(local_dest):
+    current_time = datetime.datetime.now()
+    backup_filename = current_time.strftime("remarkable_backup_%Y-%m-%d_%H-%M-%S")
+    backup_dir = os.path.join(local_dest, backup_filename)
+    os.makedirs(backup_dir, exist_ok=True)
+
+    try:
+        command = ["rmapi", "mget", "-o", backup_dir, "/"]
+        subprocess.run(command, check=True)
+        print(f"Backup of reMarkable created at {backup_dir}")
+    except subprocess.CalledProcessError as e:
+        print(f"Failed to create backup of reMarkable: {e}")
 
 def send_to_remarkable(filename, destination):
     if not os.path.isfile(filename):
@@ -17,11 +29,30 @@ def send_to_remarkable(filename, destination):
     except subprocess.CalledProcessError as e:
         print(f"Failed to send {filename} to {destination}: {e}")
 
+def send_folder_to_remarkable(folder, destination):
+    raise Exception("Does not work!")
+    """if not os.path.isdir(folder):
+        raise NotADirectoryError(f"{folder} is not a valid directory!")
+
+    try:
+        command = ["rmapi", "mput", destination, folder]
+        subprocess.run(command, check=True)
+        print(f"Recursively uploaded {folder} to reMarkable at {destination}")
+    except subprocess.CalledProcessError as e:
+        print(f"Failed to recursively upload {folder} to {destination}: {e}")"""
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Send a file to a reMarkable using rmapi.")
-    parser.add_argument("filename", help="The file to send to the reMarkable.")
-    parser.add_argument("destination", nargs="?", default="/", help="The optional destination folder on the reMarkable. Defaults to root ('/').")
+    parser.add_argument("filename", help="The file/folder to send to the reMarkable or if you want to back up the reMarkable, 'backup'.")
+    parser.add_argument("destination", nargs="?", default="/", help="The optional destination folder on the reMarkable. Defaults to root ('/'). Also the location of the back up of the reMarkable.")
 
     args = parser.parse_args()
 
-    send_to_remarkable(args.filename, args.destination)
+    if(args.filename == "backup"):
+        backup_remarkable(args.destination)
+    elif(os.path.isfile(args.filename)):
+        send_to_remarkable(args.filename, args.destination)
+    elif(os.path.isdir(args.filename)):
+        send_folder_to_remarkable(args.filename, args.destination)
+    else:
+        raise Exception("Invalid Arguments!")
